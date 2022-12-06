a07bcfaba1c4918024952f0fae4074a85018fa4a
carlosjai.me <eu@carlosjai.me>
2022-12-06 11:54:09 +0100

    Code refactor

--- src/main/java/adventofcode2022/AdventOfCode6.java
+++ src/main/java/adventofcode2022/AdventOfCode6.java
@@ -9,14 +9,14 @@ import java.util.Set;
 
 public class AdventOfCode6 {
 	public static void main(final String[] args) {
-		int part1Solution = getSolution(4);
-		int part2Solution = getSolution(14);
+		String streamBuffer = getDataStreamBuffer();
+		int part1Solution = getSolution(streamBuffer, 4);
+		int part2Solution = getSolution(streamBuffer, 14);
 		System.out.println("part 1 solution: " + part1Solution);
 		System.out.println("part 2 solution: " + part2Solution);
 	}
 
-	private static int getSolution(int numberOfCharacters) {
-		String streamBuffer = getDataStreamBuffer();
+	private static int getSolution(String streamBuffer, int numberOfCharacters) {
 		char[] dataStreamBuffer = streamBuffer.toCharArray();
 		for (int i = 0; i < dataStreamBuffer.length; i++) {
 			try {
@@ -27,7 +27,7 @@ public class AdventOfCode6 {
 				Set<Character> set = new HashSet<>(list);
 				if (set.size() == numberOfCharacters) {
 					String sequence = list.toString().replaceAll("\\[|\\]", "").replaceAll(",", "").replaceAll(" ", "");
-					return getDataStreamBuffer().indexOf(sequence) + numberOfCharacters;
+					return streamBuffer.indexOf(sequence) + numberOfCharacters;
 				}
 			} catch (IndexOutOfBoundsException ex) {
 				System.out.println("finished: sequence not found");
